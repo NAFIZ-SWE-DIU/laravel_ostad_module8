6dd72e785d0a09d8be55b3e5661142b7a6e264a5
Nafizul <nafizul35-480@diu.edu.bd>
2023-11-19 23:28:07 +0600

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Nafizul Islam";
+        $age = "25";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        setcookie($cookieName, $cookieValue, time() + ($minutes * 60), $path, $domain, $secure, $httpOnly);
+
+        return response($data)->cookie($cookieName, $cookieValue, $minutes)->setStatusCode(200);
+    }
+}
