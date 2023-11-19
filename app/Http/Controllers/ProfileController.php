1bbcce596229986ec80a9a55c98b35263490cd23
Nafizul <nafizul35-480@diu.edu.bd>
2023-11-19 23:33:55 +0600

    name change

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,7 +6,7 @@ class ProfileController extends Controller
 {
     public function index($id)
     {
-        $name = "Nafizul Islam";
+        $name = "Donal Trump";
         $age = "25";
 
         $data = [
